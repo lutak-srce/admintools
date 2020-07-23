8861af33887ca1b319b5adee66f7225612c3ded2
Ante Jurjevic <ante.jurjevic@srce.hr>
2020-07-23 11:43:01 +0200

    Add chezmoi to all distributions and fzf to Debian 9 and 10

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,43 +6,44 @@
 
 class admintools {
 
-  include ::ssh
-  include ::tools::vim
-  include ::tools::screen
-  include ::tools::tmux
-  include ::tools::tcpdump
-  include ::tools::ltrace
+  include ssh
+  include tools::ltrace
+  include tools::screen
+  include tools::tcpdump
+  include tools::tmux
+  include tools::vim
 
-  package { 'wget'            : ensure => present, }
-  package { 'rsync'           : ensure => present, }
-  package { 'nmap'            : ensure => present, }
-  package { 'zsh'             : ensure => present, }
   package { 'atop'            : ensure => present, }
-  package { 'htop'            : ensure => present, }
-  package { 'mutt'            : ensure => present, }
-  package { 'iftop'           : ensure => present, }
-  package { 'iotop'           : ensure => present, }
-  package { 'mc'              : ensure => present, }
-  package { 'ncdu'            : ensure => present, }
-  package { 'strace'          : ensure => present, }
-  package { 'telnet'          : ensure => present, }
-  package { 'lsof'            : ensure => present, }
+  package { 'bash-completion' : ensure => present, }
+  package { 'binutils'        : ensure => present, }
+  package { 'chezmoi'         : ensure => present, }
   package { 'expect'          : ensure => present, }
   package { 'gawk'            : ensure => present, }
+  package { 'htop'            : ensure => present, }
+  package { 'iftop'           : ensure => present, }
+  package { 'iotop'           : ensure => present, }
+  package { 'lsof'            : ensure => present, }
+  package { 'mc'              : ensure => present, }
+  package { 'mutt'            : ensure => present, }
+  package { 'ncdu'            : ensure => present, }
+  package { 'nmap'            : ensure => present, }
   package { 'psmisc'          : ensure => present, }
-  package { 'binutils'        : ensure => present, }
+  package { 'rsync'           : ensure => present, }
+  package { 'strace'          : ensure => present, }
   package { 'sysstat'         : ensure => present, }
-  package { 'bash-completion' : ensure => present, }
+  package { 'telnet'          : ensure => present, }
+  package { 'wget'            : ensure => present, }
+  package { 'zsh'             : ensure => present, }
 
   case $::osfamily {
     default: { }
     /(Debian|debian|Ubuntu|ubuntu)/: {
+      package { 'apticron'    : ensure => present, }
       package { 'bind9utils'  : ensure => present, }
-      package { 'dnsutils'    : ensure => present, }
-      package { 'etckeeper'   : ensure => present, }
       package { 'changetrack' : ensure => present, }
       package { 'debsums'     : ensure => present, }
-      package { 'apticron'    : ensure => present, }
+      package { 'dnsutils'    : ensure => present, }
+      package { 'etckeeper'   : ensure => present, }
       package { 'sg3-utils'   : ensure => present, }
       file { '/etc/cron.d/apticron':
         source => 'puppet:///modules/admintools/apticron/cron.d',
@@ -63,6 +64,14 @@ class admintools {
         /^7.*/: {
           package { 'man-db'    : ensure => present, }
           package { 'man-pages' : ensure => present, }
+        }
+      }
+    }
+    'Debian' : {
+      case $::operatingsystemrelease {
+        default : { }
+        /^(9|10).*/: {
+          package { 'fzf' : ensure => present, }
         }
       }
     }
